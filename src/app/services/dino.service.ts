4f4ff080ad4a8b90752cedc3d5094c10bfb60121
v.de.pontlevoye <v.de.pontlevoye@accenture.com>
2026-03-13 16:14:20 +0100

    recupération des stats des dinosaures

--- src/app/services/dino.service.ts
+++ src/app/services/dino.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Dinosaures } from '../models/Dinosaures/dinosaures.model';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DinoService {
-  private API_URL = 'http://localhost:8080';
-  private URL_SERVICE = 'dinosaures';
-  private FINAL_API = `${this.API_URL}/${this.URL_SERVICE}`;
+  private readonly API_URL = 'http://localhost:8080';
+  private readonly URL_SERVICE = 'dinosaures';
+  private readonly FINAL_API = `${this.API_URL}/${this.URL_SERVICE}`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private readonly http: HttpClient) { }
 
 getDino(id: string) {
-  return this.http.get<Dinosaures>(`http://localhost:8080/dinosaures/${id}`);
+  return this.http.get<Dinosaures>(`${this.FINAL_API}/${id}`);
 }
 
 
